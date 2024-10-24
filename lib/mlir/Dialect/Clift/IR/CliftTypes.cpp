70ad5da60c3b9286f9e4a8aa7fb80bf52a8c5edd
Lauri Vasama <vasama@rev.ng>
2024-10-24 10:17:24 +0300

    NEW: Remove clift.const

--- lib/mlir/Dialect/Clift/IR/CliftTypes.cpp
+++ lib/mlir/Dialect/Clift/IR/CliftTypes.cpp
@@ -32,20 +32,6 @@ void CliftDialect::registerTypes() {
            /* End of auto-generated list */>();
 }
 
-static mlir::Type parseConstType(mlir::AsmParser &Parser) {
-  if (Parser.parseLess().failed())
-    return {};
-
-  mlir::Type UnderlyingType;
-  if (Parser.parseType(UnderlyingType).failed())
-    return {};
-
-  if (Parser.parseGreater().failed())
-    return {};
-
-  return mlir::cast<ValueType>(UnderlyingType).addConst();
-}
-
 /// Parse a type registered to this dialect
 mlir::Type CliftDialect::parseType(mlir::DialectAsmParser &Parser) const {
   const llvm::SMLoc TypeLoc = Parser.getCurrentLocation();
@@ -54,9 +40,6 @@ mlir::Type CliftDialect::parseType(mlir::DialectAsmParser &Parser) const {
   if (mlir::Type GenType;
       generatedTypeParser(Parser, &Mnemonic, GenType).has_value())
     return GenType;
-
-  if (Mnemonic == "const")
-    return parseConstType(Parser);
 
   if (Mnemonic == ScalarTupleType::getMnemonic())
     return ScalarTupleType::parse(Parser);
